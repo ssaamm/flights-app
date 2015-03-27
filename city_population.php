f4b9bf025eca5d9a762ae51c635524cf80f78a59
Samuel Taylor <github@samueltaylor.org>
2015-03-27 17:11:24 -0500

    Factor out database creation

--- city_population.php
+++ city_population.php
@@ -1,13 +1,5 @@
 <?php
-
-try {
-    $db = new PDO('mysql:host=localhost;dbname=flights-app', 'flightsappweb', 'pw');
-    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo $e->getMessage();
-    exit;
-}
+require_once('database.php');
 
 // verify correct attributes
 if (!isset($_POST['name']) || !isset($_POST['state'])
